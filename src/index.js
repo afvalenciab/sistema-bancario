caf80134020eb8560e69740260dae720df851a2d
Andres Felipe Valencia Benitez <afvalenciab@gmail.com>
2019-12-16 18:24:41 -0600

    Creacion de readme.md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 import App from './routes/App';
 
